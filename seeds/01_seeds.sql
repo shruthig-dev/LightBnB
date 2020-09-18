a990a91bb4eaff781700a87751b097f881c2fbb0
shruthig <shruthigangadharblr@email.com>
2020-09-18 15:35:39 -0700

    Added seed data

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,27 @@
+
+INSERT INTO USERS (id , name, email, password) 
+VALUES (1,'Eva','sebastianguerray@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+(2,'Louisa','louisa@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+(3,'Sue','jasonvincent@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+(4,'Etta','charlie@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+(5,'Leroy','jaycereynolds@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+INSERT INTO properties (id,owner_id , title , description , thumbnail_photo_url , cover_photo_url , cost_per_night , parking_spaces , number_of_bathrooms , number_of_bedrooms , country , street , city , province , post_code , active )
+VALUES(1,1,'Speed lamp','description','https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&amp;cs=tinysrgb&amp;h=350','https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg','930','6','4','8','Canada ','536 Namsub Highway','Sotboske','Quebec ','28142',true),
+(2,2,'Blank corner','description','https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&amp;cs=tinysrgb&amp;h=350','https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg','85234','6','6','7','Canada ','651 Nami Road','Bohbatev ','Alberta','83680',true),
+(3,3,'Habit mix','description','https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&amp;cs=tinysrgb&amp;h=350','https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg','46058','0','5','6','Canada ','1650 Hejto Center ','Genwezuj ','Newfoundland And Labrador','44583',true);
+
+INSERT INTO reservations (id,guest_id, property_id, start_date, end_date) 
+VALUES (1,1, 1, '2018-09-11', '2018-09-26'),
+(2,2, 2, '2019-01-04', '2019-02-01'),
+(3,3, 3, '2021-10-01', '2021-10-14');
+
+INSERT INTO property_reviews(guest_id,property_id,reservation_id,rating,message)
+VALUES('1','2','1','4','messages'),
+('2','2','2','4','messages'),
+('3','3','2','4','messages'),
+('1','2','3','5','messages');
+
+
+ 
+         
